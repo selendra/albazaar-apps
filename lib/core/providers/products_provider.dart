33db1e4d748b9beb78147dd7ab65c912a748bbe2
DaveatCor <condaveat@gmail.com>
2020-11-18 10:40:25 +0700

    cleaning code in product provider file

--- lib/core/providers/products_provider.dart
+++ lib/core/providers/products_provider.dart
@@ -7,9 +7,15 @@ class ProductsProvider with ChangeNotifier {
 
   List<Product> data = List<Product>();
 
-  ProductsProvider(){
-    getData();
-  }
+  final String myImageUrl = 'https://purepng.com/public/uploads/medium/purepng.com-single-carrotcarrotonesinglevegetablesfreshdeliciousefoodhealthy-481521740676q8i3l.png';
+
+  List<Product> _vegProduct = [];
+
+  List<Product> get items => [...data];
+
+  List<Product> get vegProduct => [..._vegProduct];
+
+  Product findById(int id) => data.firstWhere((prod) => prod.id == id);
 
   Future<void> getData() async {
     await StorageServices.fetchData('fruit').then((value) async {
@@ -31,34 +37,16 @@ class ProductsProvider with ChangeNotifier {
             )
           });
         });
-        await toJson(data).then((value) async => await StorageServices.setData(value, 'fruit'));
+        await objectToJson(data).then((value) async => await StorageServices.setData(value, 'fruit'));
       } else {
-        print("Retrive data $value");
-        List.from(value).forEach((element) {
-          print(element['price'].toDouble().runtimeType.toString());
-          data.addAll({
-            Product(
-              id: element['id'].round(),
-              image: element['image'],
-              title: element['title'],
-              fruit: element['fruit'],
-              flavor: element['flavor'],
-              price: element['price'].toDouble(),
-              description: element['description'],
-              color: element['color'],
-              sellerName: element['seller_name'],
-              sellerPhoneNum: element['seller_phone'],
-              category: element['category'],
-              isFavorite: element['isFavority'] ? true : false,
-            )
-          });
-        });
+        jsonToObject(value);
       }
     });
     notifyListeners();
   }
 
-  Future<List<Map<String, dynamic>>> toJson(List<Product> data) async{
+  // Extract Data From Object To Json
+  Future<List<Map<String, dynamic>>> objectToJson(List<Product> data) async{
     List<Map<String, dynamic>> list = List<Map<String, dynamic>>();
     data.forEach((element) {
       list.add({
@@ -78,189 +66,28 @@ class ProductsProvider with ChangeNotifier {
     });
     return list;
   }
-  
 
-  final String myImageUrl = 'https://purepng.com/public/uploads/medium/purepng.com-single-carrotcarrotonesinglevegetablesfreshdeliciousefoodhealthy-481521740676q8i3l.png';
-  //LIST OF ALL PRODUCTS
-  List<Product> _items = [
-    Product(
-        id: 1,
-        title: "Apple",
-        price: 1000000,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image: "https://media.timeout.com/images/105692893/image.jpg",
-        color: 'FF3D82AE'),
-    Product(
-        id: 2,
-        title: "Avocado",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Persea_americana_fruit_2.JPG/1200px-Persea_americana_fruit_2.JPG",
-        color: 'FFD3A984'),
-    Product(
-        id: 3,
-        title: "Grapes",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://www.goodfruit.com/wp-content/uploads/marquetteGrape36018-1-1000x1264.jpg",
-        color: 'FF989493'),
-    Product(
-        id: 4,
-        title: "Lemon",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://cdn.vox-cdn.com/thumbor/AaljX--GNK0VxsnoXfexTOg2TYA=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/66928266/AdobeStock_320463231.0.0.jpeg",
-        color: 'FFE6B398'),
-    Product(
-        id: 5,
-        title: "Orange",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://www.flowerpower.com.au/media/catalog/product/image/291169a6f0/orange-valencia.jpg",
-        color: 'FFFB7883'),
-    Product(
-        id: 6,
-        title: "Banana",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://www.verywellfit.com/thmb/a4580FjTjbub9q4kI5m9X-Po-p0=/2002x1334/filters:no_upscale():max_bytes(150000):strip_icc()/Bananas-5c6a36a346e0fb0001f0e4a3.jpg",
-        color: 'FFD3A984'),
-    Product(
-      id: 7,
-      title: "Lemons",
-      price: 100,
-      description: dummyText,
-      sellerName: 'Chay',
-      sellerPhoneNum: '85776978',
-      orderQty: 1,
-      image:
-          "https://github.com/rajayogan/flutterui-fruits/blob/master/assets/lemons.png?raw=true",
-      color: 'FFAEAEAE',
-    ),
-    Product(
-        id: 8,
-        title: "Watermelon",
-        price: 1000000,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://github.com/hakymz/Flutter-Fruit-App-UI/blob/master/assets/fruits/watermelon.png?raw=true",
-        color: 'FF3D82AE'),
-    Product(
-        id: 9,
-        title: "Mango",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://www.netmeds.com/images/cms/wysiwyg/blog/2019/04/Raw_Mango_898.jpg",
-        color: 'FF989493'),
-    Product(
-        id: 10,
-        title: "Guava",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://cdn.cdnparenting.com/articles/2019/03/14174045/794794447-H-1024x700.jpg",
-        color: 'FFE6B398'),
-    Product(
-        id: 11,
-        title: "Strawberry",
-        price: 100,
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776978',
-        orderQty: 1,
-        image:
-            "https://github.com/ahkohd/flutter-day2-fruit-app-layout/blob/master/images/strawberry.png?raw=true",
-        color: 'FFFB7883'),
-    Product(
-      id: 12,
-      title: "Blueberries",
-      price: 100,
-      description: dummyText,
-      sellerName: 'Chay',
-      sellerPhoneNum: '85776978',
-      orderQty: 1,
-      image:
-          "https://github.com/rajayogan/flutterui-fruitcookbook/blob/master/assets/blueberries.png?raw=true",
-      color: 'FFAEAEAE',
-    ),
-    Product(
-        id: 13,
-        title: "Broccoli",
-        price: 100,
-        category: 'veg',
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776979',
-        orderQty: 1,
-        image:
-            'https://organiclivestockandcrops.org/wp-content/uploads/2018/03/organic-broccoli.png'),
-    Product(
-        id: 14,
-        title: "Potato",
-        price: 100,
-        category: 'veg',
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776979',
-        orderQty: 1,
-        image:
-            'https://foodimentaryguy.files.wordpress.com/2014/10/www-guildwars2roleplayers-com.png'),
-    Product(
-        id: 15,
-        title: "Carrot",
-        price: 100,
-        category: 'veg',
-        description: dummyText,
-        sellerName: 'Chay',
-        sellerPhoneNum: '85776979',
-        orderQty: 1,
-        image:
-            'https://purepng.com/public/uploads/medium/purepng.com-single-carrotcarrotonesinglevegetablesfreshdeliciousefoodhealthy-481521740676q8i3l.png'),
-  ];
-
-
-
-  List<Product> _vegProduct = [];
-
-  List<Product> get items => [..._items];
-
-  List<Product> get vegProduct => [..._vegProduct];
-
-  Product findById(int id) {
-    return _items.firstWhere((prod) => prod.id == id);
+  // Extract Data From Json To Object
+  Future<void> jsonToObject(List value) async {
+    List.from(value).forEach((element) {
+      print(element['price'].toDouble().runtimeType.toString());
+      data.addAll({
+        Product(
+          id: element['id'].round(),
+          image: element['image'],
+          title: element['title'],
+          fruit: element['fruit'],
+          flavor: element['flavor'],
+          price: element['price'].toDouble(),
+          description: element['description'],
+          color: element['color'],
+          sellerName: element['seller_name'],
+          sellerPhoneNum: element['seller_phone'],
+          category: element['category'],
+          isFavorite: element['isFavority'] ? true : false,
+        )
+      });
+    });
   }
 
   //ADD NEW PRODUCT
@@ -277,16 +104,16 @@ class ProductsProvider with ChangeNotifier {
         orderQty: 1,
         color: 'FF3D82AE'
     ));
-    toJson(data).then((value) async => { await StorageServices.setData(value, 'fruit')})
+    objectToJson(data).then((value) async => { await StorageServices.setData(value, 'fruit')})
     ;
     notifyListeners();
   }
 
   //FETCH PRODUCTS INTO DIFFERENT CATEGORIES
   void getVegi() {
-    for (int i = 0; i < _items.length; i++) {
-      if (_items[i].category == 'veg') {
-        _vegProduct.add(_items[i]);
+    for (int i = 0; i < data.length; i++) {
+      if (data[i].category == 'veg') {
+        _vegProduct.add(data[i]);
       }
 
       notifyListeners();
