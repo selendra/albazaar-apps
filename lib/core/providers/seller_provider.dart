8c2d53f051457be8963c71706b2d1698f83df3fa
Your Name <you@example.com>
2020-11-27 11:37:58 +0700

    merge chay to dev

--- lib/core/providers/seller_provider.dart
+++ lib/core/providers/seller_provider.dart
@@ -20,12 +20,7 @@ class SellerProvider with ChangeNotifier {
 
     _backend.data = json.decode(_backend.response.body);
 
-<<<<<<< HEAD
     for (var data in _backend.data) {
-      //print(data);
-=======
-    for(var data in _backend.data){
->>>>>>> 5a81c08d288d909b28ea4aa4e9aa0ace4f2a05a3
       allBuyerOrder.add(SellerModel.fromJson(data));
     }
     print(allBuyerOrder);
